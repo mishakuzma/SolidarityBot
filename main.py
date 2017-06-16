c0decc276282de02c2645383ba48a2215ea8c768
Misha Kuzma <mishakuzma@gmail.com>
2017-06-16 18:10:31 -0400

    Added response code check Need beautifulSoup integration (Get html from url to make htmlSoup object).

--- main.py
+++ main.py
@@ -13,4 +13,37 @@ Output: List of strings:
 # Use BeautifulSoup to access scraper functions
 
 import urllib3
+import json
 from bs4 import BeautifulSoup
+
+def main():
+    # ----- URL Definition for the club's page -----
+    urltodo = "http://www.testurl.com/"
+
+
+    # ----- End of URL Definition -----
+
+    # ----- Using urllib to access page elements -----
+    # Pool manager needed to use library
+    http = urllib3.PoolManager()
+
+    # check for response code before accessing page
+    r = http.request('GET',urltodo)
+    if r.status != 200:
+        return r.status
+    else:
+        pass
+
+    """
+    # extract JSON content (uncomment if relevant)
+    json.loads(r.data.dcode('utf-8')
+    
+    """
+
+    # page = urllib3.
+    # ----- End of urllib usage -----
+
+    # ----- Beautiful soup usage starts -----
+    htmlSoup = BeautifulSoup(page,'html.parser') # Need url to be opened as page with html content
+
+    # ----- Beautiful soup usage ends
